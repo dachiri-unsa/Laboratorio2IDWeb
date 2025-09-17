fe928688e077622273c9a6b3975c0748e7331e57
Xdaniel <dachiri@unsa.edu.pe>
2025-09-16 19:43:51 -0500

    funcionalidad de mayor a 50

--- ejercicio03/prueba.js
+++ ejercicio03/prueba.js
@@ -3,3 +3,9 @@ let b = 90;
 let suma;
 suma = a + b;
 console.log("La suma es: "+suma);
+if (suma > 50) {
+    console.log("El numero es mayor a 50");
+}
+else {
+    console.log("El numero es manor a 50");
+}
